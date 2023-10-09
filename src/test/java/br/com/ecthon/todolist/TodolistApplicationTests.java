4f1bfad099274c5bc33557fe26883ed7444ddf65
ecthon <ecthon@gmail.com>
2023-10-09 13:34:01 -0400

    first commit

---
+++ src/test/java/br/com/ecthon/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecthon.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
