53e610a7a9a69fa77a7e74704fcfd26bf72c061d
ecthon <ecthon@gmail.com>
2023-10-11 11:19:11 -0400

    password and user verify

--- src/main/java/br/com/ecthon/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/ecthon/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.ecthon.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.ecthon.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter{
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -35,13 +41,18 @@ public class FilterTaskAuth extends OncePerRequestFilter{
         System.out.println(password);
 
         // Validar usuário
-
-        // Validar senha
-
-        // Segue viagem
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        var user  = this.userRepository.findByUsername(username);
+        if(user == null) {
+            response.sendError(401);
+        } else {
+            // Validar senha
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if(passwordVerify.verified) {
+                filterChain.doFilter(request, response);
+            } else {
+                response.sendError(401);
+            }
+            // Segue viagem
+        }
     }
-
-    
-    
 }
