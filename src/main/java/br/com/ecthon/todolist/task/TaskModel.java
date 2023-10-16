5662380c8138fab971c75300e2c8bfaf404bab4a
ecthon <ecthon@gmail.com>
2023-10-16 08:52:12 -0400

    try catch

--- src/main/java/br/com/ecthon/todolist/task/TaskModel.java
+++ src/main/java/br/com/ecthon/todolist/task/TaskModel.java
@@ -41,4 +41,11 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50) {
+            throw new Exception("O campo de título deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
+
 }
